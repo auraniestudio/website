dffdb54d260bf1259ea4389a63fab1ba606932bc
Oranee-Kasem-Whai-25 <ornks43@gmail.com>
2026-03-05 15:44:27 +1300

    add Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
     <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
